5560cf76c5d26cf9f6de96479b9aef97f6797672
peng chuan <32349458+pchuan98@users.noreply.github.com>
2023-08-09 17:45:11 +0800

    feat: build project structure (#2)

---
+++ src/ImProcessor/Models/Image.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+
+namespace ImProcessor.Models;
+
+public class Image
+{
+    public Image(string path)
+        => _data = Cv2.ImRead(path, (ImreadModes)(-1));
+
+    internal Image(Mat mat)
+        => mat.CopyTo(_data);
+
+    /// <summary>
+    /// 处理数据，不暴露
+    /// </summary>
+    private readonly Mat _data = new();
+
+    /// <summary>
+    /// 相机元数据总和
+    /// </summary>
+    public Dictionary<string, Dictionary<string, string>>? Metadata { get; set; }
+}
